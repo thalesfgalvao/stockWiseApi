ee489dcbab55a5c552e9805ca75b58e12a687544
Thales Marques <thalesf.galvao@gmail.com>
2025-06-11 17:29:05 -0300

    Add error 404 middleware

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const itemRoute = require('./src/routes/item.route');
 const usuarioRoute = require('./src/routes/usuario.route');
+const handle404Error = require('./src/middlewares/handle404Error');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/api/usuarios', usuarioRoute);
 app.use('/api/itens', itemRoute);
+app.use(handle404Error);
 
 app.listen(process.env.PORTA, () => {
 	console.log('Rodando');
